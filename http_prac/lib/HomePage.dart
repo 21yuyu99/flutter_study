ba34158c08efabbffffb923dde9603137b29497d
21yuyu99 <yujeong0720@gmail.com>
2022-10-24 01:06:54 +0900

    puppy 사전 간단하게 정리

--- http_prac/lib/HomePage.dart
+++ http_prac/lib/HomePage.dart
@@ -10,53 +10,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   Future<List> allDog(int index) async {
-    try{
-      var url = Uri.https('api.thedogapi.com','/v1/breeds');
-      var response = await http.get(url);
-      // print('Response body: ${response.body}');
-      var decodeData = jsonDecode(response.body);
-      var list = [];
-      list.add(decodeData[0]['image']['url']);
-      return list;
-    }
-    catch(e){
-      debugPrint("failed to load");
-      return [];
-    }
+  Widget dogBox(int index1,int index2){
+    return(
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              uploadDog(index1),
+              uploadDog(index2),
+            ]
+        )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xff885aef),
         title:Text('강아지 백과사전')
       ),
       body:
-      FutureBuilder(
-        future : allDog(0),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-          if(!snapshot.hasData) return CircularProgressIndicator();
-          return Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                        width: 300,
-                        height: 300,
-                        decoration: BoxDecoration(
-                            image : DecorationImage(
-                              image:
-                              NetworkImage(snapshot.data[0]),
-                            )
-                        )
-                    )
-                  ],
-                )
-              ]
-          );
-        }
-      )
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+             //list last_index = 171
+             //무한스크롤
+              dogBox(0, 1),
+              dogBox(2,3),
+              dogBox(4, 5),
+            ],
+          )
+
     );
   }
+}
+
+
+Widget uploadDog(int index){
+  Future<Object> dogApi(int index) async {
+    try{
+      var url = Uri.https('api.thedogapi.com','/v1/breeds');
+      var response = await http.get(url);
+      // print('Response body: ${response.body}');
+      var decodeData = jsonDecode(response.body);
+      //var list = [];
+      //list.add(decodeData[0]['image']['url']);
+      var l = Map<String, dynamic>.from(decodeData[index]);
+      return l;
+    }
+    catch(e){
+      debugPrint("failed to load");
+      return {};
+    }
+  }
+  return(
+      FutureBuilder(
+          future : dogApi(index),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if(!snapshot.hasData) return CircularProgressIndicator();
+            return
+              Column(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        image : DecorationImage(
+                          image:
+                          NetworkImage(snapshot.data['image']['url']),
+                        )
+                    ),
+                  ),
+                  Text(snapshot.data['name']),
+                ],
+              );
+          }
+      )
+  );
 }
